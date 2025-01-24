08824bde0d00a273db7f0e076c5934e8d477d373
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-24 23:30:30 +0300

    Add comment to clarify PreviousPage component usage of CallList

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,5 +1,6 @@
 import CallList from '@/components/CallList';
 
+// PreviousPage component that uses the CallList component to display the previous calls of the user
 const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
