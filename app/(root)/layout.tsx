d15a95f7511ed2c07e805eff84291caff4e949b1
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-25 23:13:09 +0300

    Add comment to clarify the purpose of the RootLayout component in layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 
 import StreamVideoProvider from '@/providers/StreamClientProvider';
 
+// RootLayout component that uses the StreamVideoProvider component to wrap the main content of the app
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <main>
