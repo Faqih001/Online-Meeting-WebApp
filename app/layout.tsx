0c699c8d72914b047a5569e554864bc1ebdff62d
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-25 23:14:12 +0300

    Update comment to clarify metadata purpose for YOOM video calling app in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "@/components/ui/toaster";
 // Import the Inter font from Google Fonts and set the subsets to Latin
 const inter = Inter({ subsets: ["latin"] });
 
-// RootLayout component that uses the ClerkProvider component to wrap the main content of the app
+// Metadata for the YOOM video calling app that uses the ClerkProvider component to wrap the main content of the app
 export const metadata: Metadata = {
   title: "YOOM",
   description: "Video calling App",
