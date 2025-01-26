ca5551315a3f4af8da7be8102c13b745cb299546
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-26 23:24:39 +0300

    Add comment to clarify the DialogContent component's use of the DialogPrimitive.Content in dialog.tsx

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -34,6 +34,7 @@ const DialogOverlay = React.forwardRef<
 ));
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
+// DialogContent component that uses the DialogPrimitive.Content component
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
