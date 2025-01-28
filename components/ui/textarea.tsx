0cf79319d7f57952f2f74623ce7a226c775c1b34
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-28 22:33:51 +0300

    Add comment to clarify the Textarea component's purpose

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+// Textarea component that uses the textarea element to render the textarea component
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
