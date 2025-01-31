cc96d4b2d3af3391f706419b40392267a56af19e
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-31 23:07:45 +0300

    Add title attribute to toggle participants button for accessibility

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -94,7 +94,7 @@ const MeetingRoom = () => {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
-        <button onClick={() => setShowParticipants((prev) => !prev)}>
+        <button title="Toggle Participants" onClick={() => setShowParticipants((prev) => !prev)}>
           <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
           </div>
