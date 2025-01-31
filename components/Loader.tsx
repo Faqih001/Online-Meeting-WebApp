1bf0a20d80c0f27a466452614402f6186de063c4
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-31 23:04:54 +0300

    Add comment to clarify the purpose of the Loader component

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 
+// Loader component that renders the loader component with the loading circle image and the alt text "Loading..."
 const Loader = () => {
   return (
     <div className="flex-center h-screen w-full">
